d750f03d4342bd102d3942bf5adc9d7028dbbf69
rawan2003 <93677922+rawan2003@users.noreply.github.com>
2021-11-03 20:59:09 +0200

    Add files via upload

---
+++ first_lab.m
@@ -0,0 +1,36 @@
+a=[-7 5 -9 ; 2 -1 2; 1 -1 2];
+b=[16 3 2 13;5 10 11 8; 9 6 7 12 ; 4 15 14 1];
+d=[6 3 2; 2 12 -7 ; -1 6 2; -5 15 11];
+c=[4 2 -3 ;7 -7 9 ; 3 -5 6];
+ 3*a - 5*c;
+ %sum is can not happen because Matrix dimensions must agree
+% 7*a + 2*b;
+ 
+ c * a
+ c*d'
+ n=5;
+zeros(n)
+ m=5;
+zeros(m,n)
+ones(n)
+ones(m,n)
+size(d)
+zeros(size(d))
+diag([1 2 3 4])
+eye(n)
+%Dimensions of matrices being concatenated are not consistent.
+%[a;b];
+%[a,b];
+d=eye(7,8)
+d=d*5
+d(7,:)=d(7,:)+5
+a(7,7)=5
+
+a(1,:)
+a(:,2)
+
+
+
+
+
+
